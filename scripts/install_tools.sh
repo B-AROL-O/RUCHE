b12422dd737c1d807c82ff9154d796a2b02e2ae5
ggn0 <luigi.s1994@gmail.com>
2025-11-26 22:04:47 +0000

    Add arguments to share the bluetooth adapter on Linux

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 set -e
+echo SEARCH FOR THIS STRING!
+# Launch ros2 dependency installation script
+/workspaces/RUCHE/scripts/install_ros2_dep.sh
 
 # Install prettier
 npm install -g prettier
 
 # Print Operating System name and version
-# Launch ros2 dependency installation script
-/workspaces/RUCHE/scripts/install_ros2_dep.sh
-
 uname -a
+
 
 # EOF
