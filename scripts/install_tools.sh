d33f22bdb13546fb8725fc041b3a3a0ed93f4987
ggn0 <luigi.s1994@gmail.com>
2025-11-28 13:15:00 +0100

    Address reviewer comments
    - Fix typos
    - Remove debug echo
    - Refactor code to use relative paths

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-echo SEARCH FOR THIS STRING!
+
 # Launch ros2 dependency installation script
-/workspaces/RUCHE/scripts/install_ros2_dep.sh
+$(dirname $0)/../scripts/install_ros2_dep.sh
 
 # Install prettier
 npm install -g prettier
