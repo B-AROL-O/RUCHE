5da18f5d375857622f3e191d8f872a0c072ac528
Gianpaolo Macario <gmacario@gmail.com>
2025-11-29 10:37:54 +0100

    chore: fix Super-Linter SHELL_SHFMT errors

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -3,13 +3,12 @@
 set -e
 
 # Launch ros2 dependency installation script
-$(dirname $0)/../scripts/install_ros2_dep.sh
+$(dirname "$0")/install_ros2_dep.sh
 
 # Install prettier
 npm install -g prettier
 
 # Print Operating System name and version
 uname -a
-
 
 # EOF
