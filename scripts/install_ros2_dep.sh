06f6ffcb3ab0f3b6eeab465f63acbd4dc04ff404
Gianpaolo Macario <gmacario@gmail.com>
2025-11-29 10:45:36 +0100

    chore: fix more Super-Linter BASH errors

--- scripts/install_ros2_dep.sh
+++ scripts/install_ros2_dep.sh
@@ -3,8 +3,12 @@
 set -e
 
 sudo apt-get update
-cd $(dirname "$0")/../ros2_pkg || exit
+
+cd "$(dirname \"$0\")/../ros2_pkg" || exit
+
+# shellcheck source=/dev/null
 source /opt/ros/jazzy/setup.bash
+
 rosdep update
 rosdep install --from-paths src/ -y --ignore-src
 
